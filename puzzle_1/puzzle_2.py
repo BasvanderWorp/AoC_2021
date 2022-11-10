2455cf011e9a9cb0746f12ee163aa913172c8a38
basvanderworp <bas.van.der.worp@navara.nl>
2022-11-10 20:13:16 +0100

    first puzzle

--- puzzle_1/puzzle_2.py
+++ puzzle_1/puzzle_2.py
@@ -1,7 +1,16 @@
 
 with open('input.txt') as f:
     raw_input = f.read()
+depth_measurements = [eval(depth) for depth in raw_input.split('\n') if depth != '']
 
-depth_measurements = [eval(depth) for depth in raw_input.split('\n') if depth!='']
-depth_increments = [idx + 1 for idx, prev_depth in enumerate(depth_measurements[:-1]) if prev_depth < depth_measurements[idx+1]]
-print(len(depth_increments))
+window_size = 3
+prev_window_sum = sum(depth_measurements[0:window_size])
+number_of_increments = 0
+for idx in range(1, len(depth_measurements) - window_size + 1):
+    curr_window_sum = sum(depth_measurements[idx:idx+window_size])
+    if curr_window_sum > prev_window_sum:
+        print(f"{idx}-{idx+window_size}: {curr_window_sum} > {prev_window_sum}")
+        number_of_increments += 1
+    prev_window_sum = curr_window_sum
+
+print(number_of_increments)
