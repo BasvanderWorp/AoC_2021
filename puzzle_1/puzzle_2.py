79cb771726b6ffb9b6da0293968e52a618349015
basvanderworp <bas.van.der.worp@navara.nl>
2022-11-10 20:11:22 +0100

    first puzzle!

---
+++ puzzle_1/puzzle_2.py
@@ -0,0 +1,7 @@
+
+with open('input.txt') as f:
+    raw_input = f.read()
+
+depth_measurements = [eval(depth) for depth in raw_input.split('\n') if depth!='']
+depth_increments = [idx + 1 for idx, prev_depth in enumerate(depth_measurements[:-1]) if prev_depth < depth_measurements[idx+1]]
+print(len(depth_increments))
